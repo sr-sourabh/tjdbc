84a87962877b1e08ae29be4c1506e8e5da315832
sr-sourabh <shourabh@glabbr.com>
2021-02-19 20:04:52 +0530

    ic

---
+++ src/main/java/com/iiitb/tjdbc/Application.java
@@ -0,0 +1,9 @@
+package com.iiitb.tjdbc;
+
+import com.iiitb.tjdbc.util.Try;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(Try.test);
+    }
+}
---
+++ src/main/java/com/iiitb/tjdbc/util/Try.java
@@ -0,0 +1,5 @@
+package com.iiitb.tjdbc.util;
+
+public class Try {
+    public static final String test = "test";
+}
